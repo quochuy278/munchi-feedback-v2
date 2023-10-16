3cab60373d8ffbc856a38d32044e7df642e36308
Huy Bui <quochuy270800@gmail.com>
2023-10-16 23:06:32 +0300

    add header for feedback page

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-8 sm:h-16 w-screen flex justify-center items-center bg-white text-black">
+      Feedback
+    </div>
+  );
+};
+
+export default Header;
