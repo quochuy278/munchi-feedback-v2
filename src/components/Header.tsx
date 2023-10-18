e740b7f5a6868add38b50b0b707f04cb13ea5469
Huy Bui <quochuy270800@gmail.com>
2023-10-18 23:17:16 +0300

    modify styles in feedback header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Header = () => {
+interface HeaderProps {
+  styles?: string;
+}
+
+const FeedbackHeader = ({ styles }: HeaderProps) => {
   return (
-    <div className="h-8 sm:h-16 w-screen flex justify-center items-center bg-white text-black">
-      Feedback
+    <div
+      className="h-8 sm:h-16 w-full flex justify-center items-center bg-white text-black border-none border-solid border-2 border-sky-500"
+    >
+      <div className={styles}>Feedback</div>
     </div>
   );
 };
 
-export default Header;
+export default FeedbackHeader;
