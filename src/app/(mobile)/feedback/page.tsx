06ca0e7074fd47c9b1a1943766a4761429c4c66a
Huy Bui <quochuy270800@gmail.com>
2023-10-21 19:28:08 +0300

    modify feedback page and disable ssr

--- src/app/(mobile)/feedback/page.tsx
+++ src/app/(mobile)/feedback/page.tsx
@@ -1,16 +1,12 @@
-import FeedbackHeader from "@/components/Header";
-import FeedbackRating from "@/features/FeedbackRating/FeedbackRating";
-import React from "react";
+import dynamic from "next/dynamic";
+
+const NoSSRFeedBackRating = dynamic(
+  () => import("@/features/FeedbackRating/FeedbackRating"),
+  { ssr: false }
+);
 
 const Feedback = () => {
-  return (
-    <div className="w-screen md:max-w-sm">
-      <FeedbackHeader />
-      <div className="w-screen md:max-w-sm md:max-h-[50rem] h-[calc(100vh-32px)] sm:h-[calc(100vh-64px)] bg-white flex justify-center flex-col items-center overscroll-none touch-none">
-        <FeedbackRating />
-      </div>
-    </div>
-  );
+  return <NoSSRFeedBackRating />;
 };
 
 export default Feedback;
