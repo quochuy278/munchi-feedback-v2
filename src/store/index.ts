245e8c38ea197264112fa6aa1893f02b920e3d19
Huy Bui <quochuy270800@gmail.com>
2023-10-21 19:32:19 +0300

    add uppdate feedback in zustand store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import {
   FeedbackTemplate,
 } from "@/features/FeedbackRating/FeedbackRating.type";
 import { create } from "zustand";
-import { persist,createJSONStorage } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 const feedbacksTemplates: FeedbackTemplate[] = [
   {
@@ -25,9 +25,15 @@ interface InitialState {
   feedbacksTemplates: FeedbackTemplate[];
   feedback: Feedback[];
   addFeedback: (feedback: Feedback) => void;
+  updateFeedback: (id: number | string, feedback: Feedback) => void;
   nextPage: (value: number) => void;
   prevPage: (value: number) => void;
 }
+
+const addFeedback = (feedback: Feedback[], newFeedback: Feedback) => [
+  ...feedback,
+  newFeedback,
+];
 
 const useFeedbackStore = create<InitialState>()(
   persist(
@@ -36,21 +42,35 @@ const useFeedbackStore = create<InitialState>()(
       feedbacksTemplates: feedbacksTemplates,
       feedback: [],
       addFeedback: (feedback: Feedback) =>
-        set((state) => ({ feedback: [...state.feedback, feedback] })),
+        set((state) => ({
+          ...state.feedback,
+          feedback: addFeedback(state.feedback, feedback),
+        })),
+      updateFeedback: (id: string | number, updatedFeedback: Feedback) =>
+        set((state) => ({
+          feedback: state.feedback.map((fb) =>
+            fb.id === id ? updatedFeedback : fb
+          ),
+        })),
       nextPage: (value: number) =>
-        set((state) => ({ currentPage: state.currentPage + value })),
+        set((state) => ({
+          currentPage: Math.max(0, state.currentPage + value), // Enforce minimum 0
+        })),
       prevPage: (value: number) =>
-        set((state) => ({ currentPage: state.currentPage - value })),
+        set((state) => ({
+          currentPage: Math.max(0, state.currentPage - value),
+        })),
     }),
     {
-      name:'feedback',
+      name: "feedback",
       partialize: (state) =>
-      Object.fromEntries(
-        Object.entries(state).filter(([key]) => !['feedbacksTemplates'].includes(key))
-      ),
+        Object.fromEntries(
+          Object.entries(state).filter(
+            ([key]) => !["feedbacksTemplates"].includes(key)
+          )
+        ),
     }
   )
- );
-
+);
 
 export { useFeedbackStore };
