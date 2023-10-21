aa762930190ed03973289b9c5b8c8f7cc98a35a6
Huy Bui <quochuy270800@gmail.com>
2023-10-21 19:28:36 +0300

    Fix styles in feedback header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,8 @@ interface HeaderProps {
 
 const FeedbackHeader = ({ styles }: HeaderProps) => {
   return (
-    <div
-      className="h-8 sm:h-16 w-full flex justify-center items-center bg-white text-black border-none border-solid border-2 border-sky-500"
-    >
-      <div className={styles}>Feedback</div>
+    <div className="h-16 w-full flex justify-center items-center bg-white text-black border-b-0">
+      <div className="text-xl font-bold">Feedback</div>
     </div>
   );
 };
